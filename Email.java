2f40560c1f01a32442da1945d4c1100ba5d5f993
FlashShri <shrikantdhengle3101@gmail.com>
2023-07-18 20:52:21 +0530

    adding fields

---
+++ Email.java
@@ -0,0 +1,21 @@
+public class Email {
+
+    //fields 
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String password;
+    private int mailboxCapacity;
+
+    //constructor to initialise  firstname and lastname
+    public Email (String firstName,String lastName){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        //
+
+
+
+    }
+}
--- EmailApp.java
+++ EmailApp.java
@@ -4,6 +4,8 @@
 public class EmailApp {
 
     public static void main(String[] args) {
-        System.out.println("Email Admin App");
+        System.out.println("~~~~~~~~~~Email Admin console~~~~~~~~~~");
+
+        Email emp1 = new Email("Elon","Musk");
     }
 }
